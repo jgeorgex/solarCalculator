0fc018d8e4d99543d9a306663cd8b03264e7594b
Julian George <jules104@gmail.com>
2020-01-17 09:01:54 +0000

    Setup var's and can see expected 12 elements in console log

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 /*jslint browser:true */
 "use strict";
+
+var annaualUseKw = 0,
+  dailyUseKw = 0,
+  i = 0,
+  x = 0;
+var months = document.getElementById("mpc").getElementsByTagName("input");
+console.log(months);
